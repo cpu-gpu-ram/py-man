8ecb5ec233093a50ea4f504c8ef1cd7ec907f6a4
1Papiii <1papinoseconds@gmail.com>
2026-01-16 16:50:23 -0500

    Made the script readable bc i forgot to do that

--- install.sh
+++ install.sh
@@ -4,19 +4,25 @@ echo "Running dependency check"
 echo "(1/3) Checking for Python..."
 if pacman -Qs python > /dev/null; then
     echo "Found Python"
-else [[echo "Python not installed. Installing..."; sudo pacman -S python]]
+else
+    echo "Python not installed. Installing..."
+    sudo pacman -S python
 fi
 
 echo "(2/3) Checking for ani-cli..."
 if yay -Qs ani-cli >/dev/null; then
     echo "ani-cli is installed."
-else [[echo "ani-cli not installed. Installing..." ; yay -S ani-cli]]
+else
+    echo "ani-cli not installed. Installing..."
+    yay -S ani-cl
 fi
 
 echo "(3/3) Checking for NetworkManager..."
 if pacman -Qs networkmanager >/dev/null; then
     echo "NetworkManager found."
-else [[echo "NetworkManager not installed. Installing..." ; sudo pacman -S networkmanager]]
+else
+    echo "NetworkManager not installed. Installing..."
+    sudo pacman -S networkmanager
 fi
 
 echo "Dependency check complete!"
